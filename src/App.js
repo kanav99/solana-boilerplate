d7ecab78045b74447df797d989a70247f633c4c4
Kanav Gupta <kanav0610@gmail.com>
2021-10-05 20:02:30 +0530

    part 1: basic account information

--- src/App.js
+++ src/App.js
@@ -1,37 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   ChakraProvider,
   Box,
   Text,
-  Link,
   VStack,
-  Code,
   Grid,
   theme,
 } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+
+import * as web3 from '@solana/web3.js';
+
+const connection = new web3.Connection(
+  web3.clusterApiUrl('devnet'),
+  'confirmed'
+);
+
+const pvkey = localStorage.getItem('pvkey');
+var wallet;
+if (pvkey === null) {
+  wallet = web3.Keypair.generate();
+  localStorage.setItem('pvkey', wallet.secretKey);
+} else {
+  let arr = new Uint8Array(pvkey.replace(/, +/g, ',').split(',').map(Number));
+  wallet = web3.Keypair.fromSecretKey(arr);
+}
 
 function App() {
+  const [account, setAccount] = useState(null);
+
+  useEffect(() => {
+    async function init() {
+      let acc = await connection.getAccountInfo(wallet.publicKey);
+      setAccount(acc);
+    }
+    init();
+  }, []);
+
   return (
     <ChakraProvider theme={theme}>
       <Box textAlign="center" fontSize="xl">
         <Grid minH="100vh" p={3}>
           <ColorModeSwitcher justifySelf="flex-end" />
           <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
+            <Text>Wallet Public Key: {wallet.publicKey.toBase58()}</Text>
             <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
+              Balance:{' '}
+              {account
+                ? account.lamports / web3.LAMPORTS_PER_SOL + ' SOL'
+                : 'Loading..'}
             </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
           </VStack>
         </Grid>
       </Box>
